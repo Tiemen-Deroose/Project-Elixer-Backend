710e52db41992a9e15f07e4be15975c0eed9b006
Tiemen Deroose <tiemen.deroose@outlook.be>
2021-11-11 18:18:13 +0100

    Give mock data placeholder UUIDs

--- src/data/mock-data.js
+++ src/data/mock-data.js
@@ -1,6 +1,6 @@
 const ART_DATA = [
 	{
-        id: 1,
+        id: 'f064a797-99fa-4ec0-9454-ba14838f5df4',
         title: 'Sunlight',
         material: 'canvas',
         medium: 'resin',
@@ -8,7 +8,7 @@ const ART_DATA = [
         price: 209.99
 	},
     {
-        id: 2,
+        id: '9ee5fb36-1ff5-4588-b888-fade80b88d05',
         title: 'Autumn Tree',
         material: 'paper',
         medium: 'acrylic paint',
@@ -19,7 +19,7 @@ const ART_DATA = [
   
 const JEWELRY_DATA = [
     { 
-        id: 3,
+        id: 'ee402224-b772-4536-a378-40268b46562e',
         name: 'Gold tinted steel necklace',
         category: 'necklace',
         material: 'steel',
@@ -27,7 +27,7 @@ const JEWELRY_DATA = [
         price: 49.99
     },
     { 
-        id: 4,
+        id: '111b85dc-e7e4-473a-9952-06183f5f97cc',
         name: 'Skeletonized petals bracelet',
         category: 'bracelet',
         material: 'sterling silver',
