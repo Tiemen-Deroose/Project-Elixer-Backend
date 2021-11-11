6492f3561b82eea72f9dc546c21c07ed71931055
Tiemen Deroose <tiemen.deroose@outlook.be>
2021-11-11 18:08:04 +0100

    Add starting config

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 const Koa = require('koa');
+const config = require('config');
 const winston = require('winston');
-const MOCK_DATA = require('./data/mock-data');
 
 const app = new Koa();
+
+const NODE_ENV = config.get('env');
+const LOGGER_SILENT = config.get('logger.silent');
+
 const logger = winston.createLogger({
-    format: winston.format.simple(),
+    silent: LOGGER_SILENT,
     transports: [
       new winston.transports.Console(),
       new winston.transports.File({ filename: 'combined.log' })
     ]
-  });
+});
 
+const MOCK_DATA = require('./data/mock-data');
 
 app.use(async (ctx, next) => {
     ctx.body = MOCK_DATA;
-    logger.info('URL Request:', { message: ctx.URL });
+    logger.info('Request:', { message: ctx.URL });
     await next();
 });
 
