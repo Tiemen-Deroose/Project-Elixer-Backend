a3e842c49c5402d7fc42a8e9b59fa65e94c33164
Tiemen Deroose <tiemen.deroose@outlook.be>
2021-11-11 17:21:59 +0100

    Add winston logging

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 const Koa = require('koa');
-const app = new Koa();
-
+const winston = require('winston');
 const MOCK_DATA = require('./data/mock-data');
+
+const app = new Koa();
+const logger = winston.createLogger({
+    format: winston.format.simple(),
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({ filename: 'combined.log' })
+    ]
+  });
+
 
 app.use(async (ctx, next) => {
     ctx.body = MOCK_DATA;
-    console.log("Request: " + ctx.URL);
-    console.log("Message: " + ctx.body);
+    logger.info('URL Request:', { message: ctx.URL });
     await next();
 });
 
 app.listen(9000);
-console.log("Listening on port 9000...");
+logger.info("Listening on port 9000...");
