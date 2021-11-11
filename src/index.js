697f514841795d7cd0d583ae931b4eaaf9d27bb5
Tiemen Deroose <tiemen.deroose@outlook.be>
2021-11-11 18:21:36 +0100

    Implement basic routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const Koa = require('koa');
 const config = require('config');
 const winston = require('winston');
+const bodyParser = require('koa-bodyparser');
+const Router = require('@koa/router');
 
 const app = new Koa();
+app.use(bodyParser());
 
 const NODE_ENV = config.get('env');
 const LOGGER_SILENT = config.get('logger.silent');
@@ -15,13 +18,19 @@ const logger = winston.createLogger({
     ]
 });
 
-const MOCK_DATA = require('./data/mock-data');
+const {ART_DATA, JEWELRY_DATA} = require('./data/mock-data');
 
-app.use(async (ctx, next) => {
-    ctx.body = MOCK_DATA;
+const router = new Router();
+app.use(router.routes()).use(router.allowedMethods());
+
+router.get('/api/art', async (ctx) => {
     logger.info('Request:', { message: ctx.URL });
-    await next();
-});
+    ctx.body = ART_DATA;
+})
+router.get('/api/jewelry', async (ctx) => {
+    logger.info('Request:', { message: ctx.URL });
+    ctx.body = JEWELRY_DATA;
+})
 
 app.listen(9000);
 logger.info("Listening on port 9000...");
