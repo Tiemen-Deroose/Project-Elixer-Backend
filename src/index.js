5c2dfb4a74f903c30884d1513a00b11bc4c25d16
Tiemen Deroose <tiemen.deroose@outlook.be>
2021-11-20 17:22:33 +0100

    Implement routes with parameters in index.js

--- src/index.js
+++ src/index.js
@@ -18,19 +18,71 @@ const logger = winston.createLogger({
     ]
 });
 
-const {ART_DATA, JEWELRY_DATA} = require('./data/mock-data');
+const artService = require('./service/art');
+const jewelryService = require('./service/jewelry');
 
 const router = new Router();
 app.use(router.routes()).use(router.allowedMethods());
 
 router.get('/api/art', async (ctx) => {
-    logger.info('Request:', { message: ctx.URL });
-    ctx.body = ART_DATA;
-})
+    logger.info('GET Request:', { message: ctx.URL });
+
+    ctx.body = artService.getAll();
+});
+router.get('/api/art/:id', async (ctx) => {
+    logger.info(`GET Request:`, { message: ctx.URL });
+
+    const response = artService.getById(ctx.params.id);
+    ctx.body = response;
+});
+router.post('/api/art', async (ctx) => {
+    logger.info(`POST Request:`, { message: ctx.URL });
+
+    const response = artService.create({...ctx.request.body});
+    ctx.body = response;
+});
+router.put('/api/art/:id', async (ctx) => {
+    logger.info(`PUT Request:`, { message: ctx.URL });
+    
+    const response = artService.updateById(ctx.params.id, {...ctx.request.body});
+    ctx.body = response;
+});
+router.delete('/api/art/:id', async (ctx) => {
+    logger.info(`DELETE Request:`, { message: ctx.URL });
+    
+    const response = artService.deleteById(ctx.params.id);
+    ctx.body = response;
+});
+
 router.get('/api/jewelry', async (ctx) => {
-    logger.info('Request:', { message: ctx.URL });
-    ctx.body = JEWELRY_DATA;
-})
+    logger.info('GET Request:', { message: ctx.URL });
+
+    ctx.body = jewelryService.getAll();
+});
+router.get('/api/jewelry/:id', async (ctx) => {
+    logger.info(`GET Request:`, { message: ctx.URL });
+
+    const response = jewelryService.getById(ctx.params.id);
+    ctx.body = response;
+});
+router.post('/api/jewelry', async (ctx) => {
+    logger.info('POST Request:', { message: ctx.URL });
+
+    const response = jewelryService.create({...ctx.request.body});
+    ctx.body = response;
+});
+router.put('/api/jewelry/:id', async (ctx) => {
+    logger.info(`PUT Request:`, { message: ctx.URL });
+    
+    const response = jewelryService.updateById(ctx.params.id, {...ctx.request.body});
+    ctx.body = response;
+});
+router.delete('/api/jewelry/:id', async (ctx) => {
+    logger.info(`DELETE Request:`, { message: ctx.URL });
+    
+    const response = jewelryService.deleteById(ctx.params.id);
+    ctx.body = response;
+});
 
 app.listen(9000);
 logger.info("Listening on port 9000...");
