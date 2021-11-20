ecb14162fc5df50744962122d43989bec795af68
Tiemen Deroose <tiemen.deroose@outlook.be>
2021-11-20 17:30:48 +0100

    Add CORS & cleanup index.js ordering

--- config/production.js
+++ config/production.js
@@ -2,4 +2,9 @@ module.exports = {
     logger: {
         silent: true,
     },
+
+    cors: {
+		origins: ['http://localhost:3000'],
+		maxAge: 3 * 60 * 60,
+	},
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 const Koa = require('koa');
-const config = require('config');
-const winston = require('winston');
+const koaCors = require('@koa/cors');
 const bodyParser = require('koa-bodyparser');
 const Router = require('@koa/router');
+const winston = require('winston');
+const config = require('config');
+
+const NODE_ENV = config.get('env');
+const LOGGER_SILENT = config.get('logger.silent');
+const CORS_ORIGINS = config.get('cors.origins');
+const CORS_MAX_AGE = config.get('cors.maxAge');
 
 const app = new Koa();
 app.use(bodyParser());
 
-const NODE_ENV = config.get('env');
-const LOGGER_SILENT = config.get('logger.silent');
+app.use(
+	koaCors({
+		origin: (ctx) => {
+			if (CORS_ORIGINS.indexOf(ctx.request.header.origin) !== -1) {
+				return ctx.request.header.origin;
+			}
+			// Not a valid domain at this point, let's return the first valid as we should return a string
+			return CORS_ORIGINS[0];
+		},
+		allowHeaders: ['Accept', 'Content-Type', 'Authorization'],
+		maxAge: CORS_MAX_AGE,
+	})
+);
 
 const logger = winston.createLogger({
     silent: LOGGER_SILENT,
@@ -18,11 +35,11 @@ const logger = winston.createLogger({
     ]
 });
 
-const artService = require('./service/art');
-const jewelryService = require('./service/jewelry');
-
 const router = new Router();
 app.use(router.routes()).use(router.allowedMethods());
+
+const artService = require('./service/art');
+const jewelryService = require('./service/jewelry');
 
 router.get('/api/art', async (ctx) => {
     logger.info('GET Request:', { message: ctx.URL });
